7eb814902ee0f6db0bb9ce8e202791f648a3340f
Mohammed Mushahid Ali <alimushahid24@gmail.com>
2023-10-31 16:17:10 +0530

    Connect2Me Application :)

--- src/assets/emailCard/index.jsx
+++ src/assets/emailCard/index.jsx
@@ -21,18 +21,20 @@ export default function index({ element }) {
     );
 
     return (
-        <OverlayTrigger
-            placement="top"
-            delay={{ show: 50, hide: 50 }}
-            overlay={renderTooltip}
-        >
-            <div id={element._id} className="email-card-main my-5">
-                <span className="new-or-old">{(time <= 86400000) ? (<img src={New} alt="UNDER A DAY OLD" title="UNDER A DAY OLD" />) : ''}</span>
-                <img onClick={() => { if (confirm('You Sure?') == true) { deleteEmail(element._id) } }} src={Delete} alt="Delete" title="Click To Delete" className="delete-icon" />
-                <p>From: {element.from}</p>
-                <p>Mail Sent: {moment(atob(element.dateTime)).fromNow().toUpperCase()}</p>
-                <p>Message: {atob(element.message)}</p>
-            </div>
-        </OverlayTrigger>
+        <div>
+            <OverlayTrigger
+                placement="top"
+                delay={{ show: 50, hide: 50 }}
+                overlay={renderTooltip}
+            >
+                <div id={element._id} className="email-card-main my-5">
+                    <span className="new-or-old">{(time <= 86400000) ? (<img src={New} alt="UNDER A DAY OLD" title="UNDER A DAY OLD" />) : ''}</span>
+                    <img onClick={() => { if (confirm('You Sure?') == true) { deleteEmail(element._id) } }} src={Delete} alt="Delete" title="Click To Delete" className="delete-icon" />
+                    <p>From: {element.from}</p>
+                    <p>Mail Sent: {moment(atob(element.dateTime)).fromNow().toUpperCase()}</p>
+                    <p>Message: {atob(element.message)}</p>
+                </div>
+            </OverlayTrigger>
+        </div>
     );
 }
